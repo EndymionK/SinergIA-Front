c3ad358370290528c9e9d489689ed1082889fcf0
ibitabadger <126259117+ibitabadger@users.noreply.github.com>
2024-04-20 09:33:44 -0500

    hola

--- src/layouts/billing/components/PaymentMethod/index.js
+++ src/layouts/billing/components/PaymentMethod/index.js
@@ -17,6 +17,7 @@ Coded by www.creative-tim.com
 import Card from "@mui/material/Card";
 import Grid from "@mui/material/Grid";
 import Icon from "@mui/material/Icon";
+import Checkbox from "@mui/material/Checkbox";
 import Tooltip from "@mui/material/Tooltip";
 
 // Argon Dashboard 2 MUI components
@@ -78,9 +79,79 @@ function PaymentMethod() {
           <ArgonTypography variant="body1">Grabando</ArgonTypography>
         </ArgonBox>
       )}
-      <Dialog open={showAlert} onClose={handleCloseAlert} maxWidth="sm" fullWidth>
-        <DialogContent sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <Dialog open={showAlert} onClose={handleCloseAlert} >
+        <DialogContent >
           <Box sx={{ width: '100%' }}>
+          <ArgonBox pt={3} px={2} pb={2}>
+        <ArgonBox mb={2}>
+          <ArgonTypography
+            variant="caption"
+            color="text"
+            fontWeight="bold"
+            textTransform="uppercase"
+          >
+            Síntomas
+          </ArgonTypography>
+        </ArgonBox>
+        <Grid container >
+          <Grid item xs={1} sm={3.4} >
+            
+          <ArgonBox display="flex" alignItems="center">
+            <Checkbox checked />
+            <ArgonTypography variant="body1">Dificultad para respirar</ArgonTypography>
+          </ArgonBox>
+       
+            <ArgonBox display="flex" alignItems="center">
+              <Checkbox checked />
+              <ArgonTypography variant="body1">Dolor en el pecho</ArgonTypography>
+            </ArgonBox>
+  
+            <ArgonBox display="flex" alignItems="center">
+              <Checkbox />
+              <ArgonTypography variant="body1">Vómitos persistentes</ArgonTypography>
+            </ArgonBox>
+          </Grid>
+          <Grid item xs={12} sm={4} >
+   
+            <ArgonBox display="flex" alignItems="center">
+              <Checkbox checked/>
+              <ArgonTypography variant="body1">Coloración azulada en los labios o la cara (cianosis)</ArgonTypography>
+            </ArgonBox>
+            <ArgonBox display="flex" alignItems="center">
+              <Checkbox />
+              <ArgonTypography variant="body1">Dificultad para hablar o moverse</ArgonTypography>
+            </ArgonBox>
+            <ArgonBox display="flex" alignItems="center">
+              <Checkbox checked/>
+              <ArgonTypography variant="body1">Sensación de presión en el pecho</ArgonTypography>
+            </ArgonBox>
+            <ArgonBox display="flex" alignItems="center">
+              <Checkbox />
+              <ArgonTypography variant="body1">Dolor abdominal intenso</ArgonTypography>
+            </ArgonBox>
+          </Grid>
+          <Grid item xs={12} sm={4} >
+            <ArgonBox display="flex" alignItems="center">
+              <Checkbox />
+              <ArgonTypography variant="body1">Diarrea grave</ArgonTypography>
+            </ArgonBox>
+            <ArgonBox display="flex" alignItems="center">
+              <Checkbox />
+              <ArgonTypography variant="body1">Mareos o desmayos</ArgonTypography>
+            </ArgonBox>
+            <ArgonBox display="flex" alignItems="center">
+              <Checkbox />
+              <ArgonTypography variant="body1">Debilidad repentina o entumecimiento en un lado del cuerpo</ArgonTypography>
+            </ArgonBox>
+            <ArgonBox display="flex" alignItems="center">
+              <Checkbox checked/>
+              <ArgonTypography variant="body1">Cambios repentinos en la visión</ArgonTypography>
+            </ArgonBox>
+          </Grid>
+        </Grid >
+      </ArgonBox >
+            {/* //-------- */}
+
             <Alert severity="warning" onClose={handleCloseAlert} variant="outlined" sx={{ width: '100%', borderRadius: 0 }}>
               <AlertTitle>¡Alerta!</AlertTitle>
               Enviar a UCI
@@ -88,6 +159,7 @@ function PaymentMethod() {
           </Box>
         </DialogContent>
       </Dialog>
+
     </Card>
   );
 }
